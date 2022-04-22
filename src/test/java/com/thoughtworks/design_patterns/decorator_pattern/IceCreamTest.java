d98c00588c5a46763aeb2c0b551517cdb3c80d8c
Yazhini <yazhini.v@thoughtworks.com>
2022-04-22 06:58:19 +0530

    Add assertion in test

--- src/test/java/com/thoughtworks/design_patterns/decorator_pattern/IceCreamTest.java
+++ src/test/java/com/thoughtworks/design_patterns/decorator_pattern/IceCreamTest.java
@@ -2,11 +2,13 @@ package com.thoughtworks.design_patterns.decorator_pattern;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class IceCreamTest {
     @Test
     void shouldReturnTheCostBasedOnTheFlavourAndTheNoOfScoopsChosenByTheCustomer() {
         IceCream iceCream = new Chocolate(2);
-        System.out.println(iceCream.description());
-        System.out.println("Cost : Rs."+iceCream.cost());
+        assertEquals("You have chosen Chocolate with 2 scoops", iceCream.description());
+        assertEquals(150.0, iceCream.cost());
     }
 }
